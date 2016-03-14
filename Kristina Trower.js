ea91b9112781014463a9ee53fe4d0916c21902e4
kjtrower <mickeymousehouse@att.net>
2016-03-14 19:49:47 -0400

    initial project version

---
+++ Kristina Trower.js
@@ -0,0 +1,9 @@
+confirm("Are you intersted in learning about Kristina?");
+var age=prompt("What is your age?");
+console.log(age);
+if (age >16)
+{confirm("You are "+age+" and over the age of 16, congrats! Read all about her!");
+}
+else
+{confirm("You are only "+age+", go play outside!");
+}
